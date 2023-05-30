aa9fb13e83926b2db6dc5a4d89e35774d7a7ba54
farhantanvirtushar <farhantanvir.2340.csedu@gmail.com>
2023-05-31 00:14:41 +0600

    Add model for home menu

---
+++ src/app/models/Template.ts
@@ -0,0 +1,5 @@
+export interface Template {
+  url:string;
+  isPro: boolean;
+  image:string;
+}
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Template } from 'src/app/models/Template';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  templetList: Template[] =[
+    {
+      url:'template_1',
+      isPro: false,
+      image:'template_1.png'
+    }
+  ];
   constructor() { }
 
   ngOnInit(): void {
